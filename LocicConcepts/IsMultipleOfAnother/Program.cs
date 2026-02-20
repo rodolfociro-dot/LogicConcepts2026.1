b6201eca6d9503da80a5ad0dfe1289e0246bc8c1
RODOLFO ALPIDIO CIRO HERNANDEZ <rodolfo.ciro@gmail.com>
2026-02-19 19:15:09 -0500

    Exercise 04 Completed

---
+++ LocicConcepts/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,33 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    Console.WriteLine("Ingrese 2 números diferentes...");
+    var a = ConsoleExtension.GetInt("Ingrese primer número : ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+    if (a == b)
+    {
+        Console.WriteLine("Deben ser diferentes, vuelva a empezar...");
+        continue;
+    }
+
+    if (a % b == 0) 
+    {
+        Console.WriteLine($"El numero {a}, es múltiplo de {b}");
+    }
+    else
+    {
+        Console.WriteLine($"El numero {a}, No es múltiplo de {b}");
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
+
